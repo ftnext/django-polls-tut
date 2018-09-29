ee80f7ac4e2b76f52b04bed07ffb5ed45de6806d
ftnext <takuyafjp+develop@gmail.com>
2018-09-29 16:20:36 +0900

    [feat] その4：ビューのvoteメソッド（投票機能）を実装
    * ref: https://docs.djangoproject.com/ja/2.1/intro/tutorial04/#write-a-simple-form

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,11 @@
 from django.http import Http404
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.urls import reverse
 
+from .models import Choice
 from .models import Question
 
 
@@ -20,4 +23,15 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
